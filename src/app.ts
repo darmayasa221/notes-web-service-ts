30aaf1e0388da48d1758e450e7b43befb7b33792
Darma Yasa <darmayasa@192.168.204.103>
2022-10-23 04:54:53 +0800

    hello world express

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,14 @@
-console.log("a");
-export {};
+import express from "express";
+
+const app = express();
+const port = 5000;
+const host = process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0";
+
+app.get("/", (req, res) => {
+  res.send("hellow world");
+});
+
+app.listen(port, host, () => {
+  // eslint-disable-next-line no-console
+  console.log(`listen on ${host}:${port}`);
+});
